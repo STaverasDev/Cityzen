462bf37b3e499e8ce64ae3445a1d062c3e0e59a6
ShantalT <shantaltaveras@ac.c4q.nyc>
2018-05-13 18:00:22 -0400

    add request for permission in mapsactivity

--- app/src/main/java/nyc/c4q/cityzenapp/MapsActivity.java
+++ app/src/main/java/nyc/c4q/cityzenapp/MapsActivity.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -29,6 +33,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private RecyclerView recyclerView;
     private CollectPresenter presenter;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private final static int MAP_ZOOM = 24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,20 +66,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1020);
         } else {
-            // Show rationale and request permission.
-            // Add a marker in Sydney and move the camera
-            LatLng sydney = new LatLng(-34, 151);
-            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap.setMyLocationEnabled(true);
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations, this can be null.
+                            double lat = location.getLatitude();
+                            double lng = location.getLongitude();
+                            LatLng curr = new LatLng(lat, lng);
+                            CameraPosition cameraPosition = new CameraPosition.Builder().target(curr).zoom(MAP_ZOOM).build();
+                            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Current Location"));
+                            if (location != null) {
+                                // Logic to handle location object
+                            }
+                        }
+                    });
         }
 
     }
-    
+
     @Override
     public void showMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
